8956b9722fa63b6ae4233ba80ea32e3dae95865c
David .C Gomes <52863043+MaeinesenieaM@users.noreply.github.com>
2023-08-24 20:08:32 -0300

    Add files via upload

---
+++ Ex08Reajuste.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int main ()
+{
+   float salario;
+   float reajuste;
+
+   setlocale (LC_ALL, "Portuguese");
+
+   printf ("----Reajuste de salário----\n\n");
+   printf (" Insira o salário atual: "); scanf ("%f", &salario);
+   printf (" Insire o percentual de reajuste: "); scanf ("%f", &reajuste);
+
+   //Calculo para o reajuste.
+   reajuste = (reajuste / 100) + 1;
+   salario = salario * reajuste;
+
+   printf (" .\n .\n .\n O Salário sera em torno de R$ %.2f", salario);
+
+   printf ("\n\n    __o  bici! \n  _/><_ \n (_)/(_) \n  "); //BICI!!!
+
+   return 0;
+}
