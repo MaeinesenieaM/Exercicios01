f7ad0f92220f329e110e2dc9a3d33160c30d63bd
David .C Gomes <52863043+MaeinesenieaM@users.noreply.github.com>
2023-08-24 20:24:19 -0300

    Add files via upload

---
+++ Ex09Carro.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main ()
+{
+   float dist = 0.28; //28% da distribuidora
+   float imp = 0.45;  // 45% de imposto
+   float pag;         // preço para a fabricar o carro
+   float preco;       // preço total
+
+   setlocale (LC_ALL, "Portuguese");
+
+   printf ("----Fabrica De Carros----\n\n");
+   printf (" Insira o dinheiro para fabricar o carro: "); scanf ("%f", &pag);
+   printf (" .\n");
+
+   preco = pag * (dist + imp + 1);
+
+   printf (" .\n");
+   printf (" Preco total do carro: R$ %.2f", preco);
+
+   return 0;
+}
