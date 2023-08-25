cfa737527f296f6a7b2ebf961ff403f0a3bf8d96
David .C Gomes <52863043+MaeinesenieaM@users.noreply.github.com>
2023-08-25 10:13:38 -0300

    Add files via upload

--- Ex05Mercado.c
+++ Ex05Mercado.c
@@ -3,16 +3,16 @@
 
 int main ()
 {
-   int vmercadoria, vtaxa, venda, gastos, ganhos;
+   float vmercadoria, vtaxa, venda, gastos, ganhos;
    float porcentagem;
 
    setlocale (LC_ALL, "Portuguese");
 
    printf ("======Calculadora de ganhos do mercado======\n\n");
 
-   printf (" O valor da mercadoria: "); scanf ("%d", &vmercadoria);
-   printf (" O custo da taxa e despesas: "); scanf ("%d", &vtaxa);
-   printf (" Agora insira o preço desejado do produto: "); scanf ("%d", &venda);
+   printf (" O valor da mercadoria: "); scanf ("%f", &vmercadoria);
+   printf (" O custo da taxa e despesas: "); scanf ("%f", &vtaxa);
+   printf (" Agora insira o preço desejado do produto: "); scanf ("%f", &venda);
 
    gastos = vmercadoria + vtaxa;
    ganhos = venda - gastos;
